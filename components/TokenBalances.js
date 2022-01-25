0ed3af9f62c21c902d3fb455e744b9b6ef8fa00f
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-01-25 01:49:08 +0100

    displays gov tokens

--- components/TokenBalances.js
+++ components/TokenBalances.js
@@ -5,9 +5,10 @@ import karmicContract from '../contracts/Karmic.json'
 import erc20 from '../contracts/ERC20.json'
 
 const TokenBalances = () => {
-  const [fetchingBoxTokens, setFetchingBoxTokens] = useState(true)
+  const [fetchingTokens, setFetchingTokens] = useState(true)
   // [address, balance, status]
   const [tokens, setTokens] = useState([])
+  const [govTokenBalances, setGovTokenBalances] = useState([])
   const { state } = useContext(Web3Context)
   const { web3Provider, address } = state
 
@@ -19,10 +20,10 @@ const TokenBalances = () => {
     )
 
     const fetchTokenBalances = async () => {
-      const boxTokens = await karmicInstance.getBoxTokens()
+      const boxTokenAddresses = await karmicInstance.getBoxTokens()
 
-      const tokens = await Promise.all(
-        boxTokens.map(async (token) => {
+      const boxTokens = await Promise.all(
+        boxTokenAddresses.map(async (token) => {
           const tokenInstance = new ethers.Contract(
             token,
             erc20.abi,
@@ -44,8 +45,13 @@ const TokenBalances = () => {
         })
       )
 
-      setTokens(tokens)
-      setFetchingBoxTokens(false)
+      const govTokenBalances = (
+        await karmicInstance.allBalancesOf(address)
+      ).map((balance) => ethers.utils.formatEther(balance))
+
+      setTokens(boxTokens)
+      setGovTokenBalances(govTokenBalances)
+      setFetchingTokens(false)
     }
 
     if (web3Provider) {
@@ -97,44 +103,56 @@ const TokenBalances = () => {
   }
 
   const claimableTokens = tokens.filter((token) => token.balance > 0)
-
   return (
     <div>
       <h1>Tokens</h1>
-      {fetchingBoxTokens ? (
-        <div>fetching tokens..</div>
+      {fetchingTokens ? (
+        <p>fetching box tokens..</p>
       ) : (
         <div>
           <h2>Box Tokens</h2>
           {claimableTokens.length > 0 ? (
-            claimableTokens.map((tokenBalance) => {
-              const { token, balance, status } = tokenBalance
-              return (
-                <>
-                  <div key={token}>
-                    <span>{token}</span>: <span>{balance}</span>{' '}
-                    <button
-                      onClick={() => handleApprove(tokenBalance)}
-                      disabled={status != 'initialized'}
-                    >
-                      {status == 'pending'
-                        ? 'pending approval'
-                        : status == 'approved'
-                        ? 'Approved'
-                        : 'Approve'}
-                    </button>
-                  </div>
-                </>
-              )
-            })
+            <>
+              {claimableTokens.map((tokenBalance) => {
+                const { token, balance, status } = tokenBalance
+                return (
+                  <>
+                    <div key={token}>
+                      <span>{token}</span>: <span>{balance}</span>{' '}
+                      <button
+                        onClick={() => handleApprove(tokenBalance)}
+                        disabled={status && status != 'initialized'}
+                      >
+                        {status == 'pending'
+                          ? 'pending approval'
+                          : status == 'approved'
+                          ? 'Approved'
+                          : 'Approve'}
+                      </button>
+                    </div>
+                  </>
+                )
+              })}
+              <button onClick={() => handleClaim()}>Claim</button>
+            </>
           ) : (
             <p>No tokens to claim</p>
           )}
-          <button onClick={() => handleClaim()}>Claim</button>
         </div>
       )}
-
-      {}
+      {fetchingTokens ? (
+        <p>fetching gov tokens..</p>
+      ) : (
+        <div>
+          <h2>Gov Tokens</h2>
+          {govTokenBalances.length > 0 &&
+            govTokenBalances.map((balance, idx) => (
+              <div key={idx}>
+                <span>{`gov_tier_${idx + 1}: `}</span> <span>{balance}</span>
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   )
 }
