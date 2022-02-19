18acb1b93841514e0a34e55d9315e855a9acdecd
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-02-19 13:09:40 +0530

    added css files

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import Web3ContextProvider from '../context/Web3Context'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../components/assets/css/DappNav.css';
-import '../components/assets/css/Global.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../components/assets/css/CTACard.css'
+import '../components/assets/css/DappNav.css'
+import '../components/assets/css/KarmicModal.css'
+import '../components/assets/css/Global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Web3ContextProvider>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <Component {...pageProps} />
     </Web3ContextProvider>
   )
