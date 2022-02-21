0df5f2eca3541c6840193f1a19965d0f52c2562e
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-02-21 17:16:22 +0530

    now the balance is rounded off to lowest value

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -1,8 +1,12 @@
-import { formatUnits } from 'ethers/lib/utils'
+import { formatUnits, parseUnits } from 'ethers/lib/utils'
 import { useContext } from 'react'
 import { Web3Context } from '../context/Web3Context'
 import { ellipseAddress, getChainData } from '../lib/utilities'
 import Button from './Button'
+
+const roundOffBN = (number) => {
+  return formatUnits(number.div(parseUnits("0.01",18)), 2);
+}
 
 const ConnectWallet = ({ ethBalance, karmicBalance }) => {
   const { state, connect, disconnect } = useContext(Web3Context)
@@ -16,10 +20,10 @@ const ConnectWallet = ({ ethBalance, karmicBalance }) => {
         address && (
           <div className="wallet-container rounded-purple-container bg-transparent">
             <span>
-              {karmicBalance ? formatUnits(karmicBalance, 18) : '-'} Karmic
+              {karmicBalance ? roundOffBN(karmicBalance) : '-'} Karmic
             </span>
             <span style={{ marginLeft: '10px', marginRight: '10px' }}>|</span>
-            <span>{ethBalance ? formatUnits(ethBalance, 18) : '-'} ETH</span>
+            <span>{ethBalance ? roundOffBN(ethBalance) : '-'} ETH</span>
             <span style={{ marginLeft: '10px', marginRight: '10px' }}>|</span>
             <span>{ellipseAddress(address)}</span>
           </div>
