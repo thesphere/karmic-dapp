9b6a850199f0dc5e1a3c749c920ef661db987547
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-03-03 23:52:00 +0530

    formatted code and removed unnecessary console.logs

--- components/TokenBalances.js
+++ components/TokenBalances.js
@@ -20,23 +20,26 @@ const TokenBalances = () => {
   const { web3Provider, address } = state
 
   const fetchTokenBalances = async () => {
-    const boxTokenAddresses = (await karmicInstance.getBoxTokens()).filter(address=>address!=="0x82939D7ce86c81a63BCeA2B287947AE6940ba8C3")
+    const boxTokenAddresses = (await karmicInstance.getBoxTokens()).filter(
+      (address) => address !== '0x82939D7ce86c81a63BCeA2B287947AE6940ba8C3'
+    )
 
     let boxTokens = await Promise.all(
       boxTokenAddresses.map(async (token) => {
         const isBoxToken = token != '0x0000000000000000000000000000000000000000'
-        console.log(isBoxToken, token);
         let balance = ethers.BigNumber.from(0),
           isKarmicApproved = ethers.BigNumber.from(0)
         let name
         if (isBoxToken) {
-          console.log('no error')
           const tokenInstance = new ethers.Contract(
             token,
             erc20.abi,
             web3Provider
           )
-          name = token === '0xe80D26244b61B3e4ff3e222FC6506d7922ebac3B' ? 'Seed #1: PLI' : await tokenInstance.name()
+          name =
+            token === '0xe80D26244b61B3e4ff3e222FC6506d7922ebac3B'
+              ? 'Seed #1: PLI'
+              : await tokenInstance.name()
           balance = await tokenInstance.balanceOf(state.address)
 
           isKarmicApproved = await tokenInstance.allowance(
@@ -47,7 +50,6 @@ const TokenBalances = () => {
         const boxToken = await karmicInstance.boxTokenTiers(token)
         const metadataUrl = await karmicInstance.uri(boxToken.id)
         let image
-        console.log(metadataUrl, boxToken.id, isBoxToken, token);
         if (isBoxToken) {
           const targetUrl =
             metadataUrl.slice(0, 4) === 'ipfs'
@@ -135,7 +137,6 @@ const TokenBalances = () => {
     tokensCopy.reduce((previousApprove, tokenCopy, index) => {
       return previousApprove
         .then(() => {
-          console.log(index)
           console.log('Approving token ', tokenCopy.token)
           return handleApprove(tokenCopy)
         })
